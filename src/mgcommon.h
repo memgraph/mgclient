3821b064db2ac7db34497501da62e16b2a047ae4
Marko Budiselic <marko.budiselic@memgraph.com>
2020-11-21 11:30:55 +0100

    Fix formatting and remove Docker from macos

--- src/mgcommon.h
+++ src/mgcommon.h
@@ -34,10 +34,10 @@
 #define be64toh(x) OSSwapBigToHostInt64(x)
 #define le64toh(x) OSSwapLittleToHostInt64(x)
 
-#define __BYTE_ORDER    BYTE_ORDER
-#define __BIG_ENDIAN    BIG_ENDIAN
+#define __BYTE_ORDER BYTE_ORDER
+#define __BIG_ENDIAN BIG_ENDIAN
 #define __LITTLE_ENDIAN LITTLE_ENDIAN
-#define __PDP_ENDIAN    PDP_ENDIAN
+#define __PDP_ENDIAN PDP_ENDIAN
 
 #define MG_RETRY_ON_EINTR(expression)          \
   ({                                           \
