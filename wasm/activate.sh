bf1cc157dd76aaf6d365c6ec6046efdbf9e94439
Kostas <kostaskyrim@gmail.com>
2022-03-08 23:13:46 +0200

    Added scripts for wasm deps

---
+++ wasm/activate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/opt/mgclient/emsdk/emsdk activate latest
+source /opt/mgclient/emsdk/emsdk_env.sh
---
+++ wasm/install_deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/emscripten-core/emsdk.git /opt/mgclient/emsdk
+/opt/mgclient/emsdk/emsdk install latest
+
