2b8e52b4c3e9ea31dfd8477644ebc02526d11fdd
Kostas <kostaskyrim@gmail.com>
2022-03-10 16:46:09 +0200

    fix CI and improvements

--- wasm/install_deps.sh
+++ wasm/install_deps.sh
@@ -3,3 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git clone https://github.com/emscripten-core/emsdk.git ${DIR}/../build/emsdk
 ${DIR}/../build/emsdk/emsdk install latest
+${DIR}/../build/emsdk/emsdk activate latest
