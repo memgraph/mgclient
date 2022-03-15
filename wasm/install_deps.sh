100e98fbd558b8ff7d76104500146bd520cdcc56
Kostas <kostaskyrim@gmail.com>
2022-03-15 14:41:26 +0200

    added install deps config step in cmake for wasm build

--- wasm/install_deps.sh
+++ wasm/install_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-git clone https://github.com/emscripten-core/emsdk.git ${DIR}/../build/emsdk
-${DIR}/../build/emsdk/emsdk install latest
-${DIR}/../build/emsdk/emsdk activate latest
+git clone https://github.com/emscripten-core/emsdk.git ${DIR}/emsdk
+${DIR}/emsdk/emsdk install latest
+${DIR}/emsdk/emsdk activate latest
