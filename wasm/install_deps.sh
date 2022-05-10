67d658282bf683f6ec4fb107f42bcf95185243ae
Kostas <kostaskyrim@gmail.com>
2022-05-10 13:39:03 +0300

    Add emsdk 3.9 instead of latest in wasm install deps

--- wasm/install_deps.sh
+++ wasm/install_deps.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git clone https://github.com/emscripten-core/emsdk.git ${DIR}/emsdk
-${DIR}/emsdk/emsdk install latest
-${DIR}/emsdk/emsdk activate latest
+${DIR}/emsdk/emsdk install sdk-releases-upstream-edabe25af34554d19c046078f853999b074259ca-64bit
+${DIR}/emsdk/emsdk activate sdk-releases-upstream-edabe25af34554d19c046078f853999b074259ca-64bit
