8339e010d26ed3c7637011f2ea4dff30aa37658d
Kostas <kostaskyrim@gmail.com>
2022-05-10 14:28:29 +0300

    Specify version instead of commit

--- wasm/install_deps.sh
+++ wasm/install_deps.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git clone https://github.com/emscripten-core/emsdk.git ${DIR}/emsdk
-${DIR}/emsdk/emsdk install sdk-releases-upstream-edabe25af34554d19c046078f853999b074259ca-64bit
-${DIR}/emsdk/emsdk activate sdk-releases-upstream-edabe25af34554d19c046078f853999b074259ca-64bit
+${DIR}/emsdk/emsdk install 3.1.9
+${DIR}/emsdk/emsdk activate 3.1.9
