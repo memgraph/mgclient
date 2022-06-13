33b46746652fd1cf126e67aa2f1defb54fc12016
János Benjamin Antal <benjamin.antal@memgraph.io>
2022-06-13 23:03:54 +0200

    Ignore unknown warning option to make gmock_wrapper compile with older compilers

--- tests/gmock_wrapper.h
+++ tests/gmock_wrapper.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #pragma GCC diagnostic push
+// On older versions of clang/gcc deprecated-copy might not exist
+#pragma GCC diagnostic ignored "-Wunknown-warning-option"
 #pragma GCC diagnostic ignored "-Wdeprecated-copy"
 #include <gmock/gmock.h>
 #pragma GCC diagnostic pop
